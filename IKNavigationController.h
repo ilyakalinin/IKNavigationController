6d2a73539755a4f10761ab8bd78291ce50516124
Ilya <il.kalinin@gmail.com>
2014-06-09 15:11:57 +0300

    Create IKNavigationController.h

---
+++ IKNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  IKNavigationController.h
+//  
+//
+//  Created by Ilya Kalinin on 5/15/14.
+//  Copyright (c) 2014 Ilya Kalinin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKNavigationController : UINavigationController
+
+@end
